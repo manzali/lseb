f98d6572550ea9f37cb27530e7d90973e801a3ef
Matteo Manzali <matteo.manzali@gmail.com>
2015-03-03 17:44:08 +0100

    Added warning on missing space for events generation and blocking wait to the shared queue.

--- ru/controller.cpp
+++ ru/controller.cpp
@@ -24,6 +24,7 @@ void Controller::operator()(size_t generator_frequency) {
   auto const start_time = std::chrono::high_resolution_clock::now();
   size_t tot_generated_events = 0;
   auto current_metadata = m_metadata_range.begin();
+  bool wait_for_events = false;
 
   while (true) {
     double const elapsed_seconds = std::chrono::duration<double>(
@@ -33,24 +34,32 @@ void Controller::operator()(size_t generator_frequency) {
 
     assert(tot_events_to_generate >= tot_generated_events);
 
-    size_t const generated_events = m_generator.generateEvents(
-        tot_events_to_generate - tot_generated_events);
+    // If there are events to generate
+    if (tot_events_to_generate != tot_generated_events) {
 
-    // Send generated events
-    if (generated_events) {
-      auto previous_metadata = current_metadata;
-      current_metadata = advance_in_range(current_metadata, generated_events,
-                                          m_metadata_range);
-      m_ready_events_queue.push(
-          MetaDataRange(previous_metadata, current_metadata));
-      tot_generated_events += generated_events;
+      size_t const generated_events = m_generator.generateEvents(
+          tot_events_to_generate - tot_generated_events);
+
+      // Send generated events
+      if (generated_events) {
+        auto previous_metadata = current_metadata;
+        current_metadata = advance_in_range(current_metadata, generated_events,
+                                            m_metadata_range);
+        m_ready_events_queue.push(
+            MetaDataRange(previous_metadata, current_metadata));
+        tot_generated_events += generated_events;
+      } else {
+        LOG(WARNING) << "Not enough space for events generation!";
+        wait_for_events = true;
+      }
     }
 
     // Receive events to release
-    while (!m_sent_events_queue.empty()) {
+    while (wait_for_events || !m_sent_events_queue.empty()) {
       size_t const events_to_release = distance_in_range(
           m_sent_events_queue.pop(), m_metadata_range);
       m_generator.releaseEvents(events_to_release);
+      wait_for_events = false;
     }
   }
 
