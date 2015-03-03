3f47d41f27e4c7a2e21b0199dd21a7296d4587af
Matteo Manzali <matteo.manzali@gmail.com>
2015-03-03 12:31:37 +0100

    Implemented close functionality in shared queue.

--- common/shared_queue.h
+++ common/shared_queue.h
@@ -9,35 +9,56 @@
 template<typename T>
 class SharedQueue {
 
- private:
-  std::queue<T> queue_;
-  std::mutex mutex_;
-  std::condition_variable cond_;
+  std::queue<T> m_queue;
+  std::mutex m_mutex;
+  std::condition_variable m_cond;
+  bool m_closed;
 
  public:
+
   T pop() {
-    std::unique_lock<std::mutex> mlock(mutex_);
-    while (queue_.empty()) {
-      cond_.wait(mlock);
+    std::unique_lock<std::mutex> mlock(m_mutex);
+    while (m_queue.empty()) {
+      m_cond.wait(mlock);
     }
-    T item(queue_.front());
-    queue_.pop();
+    if (m_closed) {
+      m_cond.notify_one();
+      throw std::runtime_error(
+          "Trying to retrieve an item from a closed queue.");
+    }
+    T item(m_queue.front());
+    m_queue.pop();
     return item;
   }
 
   void push(const T& item) {
-    std::unique_lock<std::mutex> mlock(mutex_);
-    queue_.push(item);
-    cond_.notify_one();
+    assert(!m_closed && "Trying to insert item in a closed queue.");
+    std::unique_lock<std::mutex> mlock(m_mutex);
+    m_queue.push(item);
+    m_cond.notify_one();
   }
 
   bool empty() {
-    std::unique_lock<std::mutex> mlock(mutex_);
-    return queue_.empty();
+    std::unique_lock<std::mutex> mlock(m_mutex);
+    return m_queue.empty();
   }
 
-  SharedQueue() = default;
-  SharedQueue(const SharedQueue&) = delete;            // disable copying
+  void close() {
+    std::unique_lock<std::mutex> mlock(m_mutex);
+    m_closed = true;
+    m_cond.notify_one();
+  }
+
+  size_t size() const {
+    std::unique_lock<std::mutex> mlock(m_mutex);
+    return m_queue->size();
+  }
+
+  SharedQueue()
+      : m_closed(0) {
+  }
+
+  SharedQueue(const SharedQueue&) = delete;             // disable copying
   SharedQueue& operator=(const SharedQueue&) = delete;  // disable assignment
 
 };
