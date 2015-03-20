a9b32ae83cbdeeeb564915a446b5ec2e51734a8e
Matteo Manzali <matteo.manzali@gmail.com>
2015-03-20 16:07:04 +0100

    Implemented DataRange and MetaDataRange in terms of templated Range. Implemented DataBuffer and MetaDataBuffer in terms of templated Buffer.

--- common/dataformat.h
+++ common/dataformat.h
@@ -11,7 +11,8 @@ struct EventMetaData {
   uint64_t id;
   uint64_t length;
   EventMetaData(uint64_t id, uint64_t length)
-      : id(id),
+      :
+        id(id),
         length(length) {
   }
 };
@@ -21,23 +22,31 @@ struct EventHeader {
   uint64_t length;
   uint64_t flags;
   EventHeader(uint64_t id, uint64_t length, uint64_t flags)
-      : id(id),
+      :
+        id(id),
         length(length),
         flags(flags) {
   }
 };
 
-class MetaDataRange {
-  EventMetaData* m_begin;
-  EventMetaData* m_end;
+template<typename T>
+class Range {
+ public:
+  using value_type = T;
+  using iterator = T*;
+  using const_iterator = T const*;
+
+ private:
+  iterator m_begin;
+  iterator m_end;
 
  public:
-  using value_type = EventMetaData;
-  using iterator = EventMetaData*;
-  using const_iterator = EventMetaData const*;
-  MetaDataRange(EventMetaData* begin, EventMetaData* end)
-      : m_begin(begin),
+  Range(iterator begin, iterator end)
+      :
+        m_begin(begin),
         m_end(end) {
+    assert(is_aligned_for<T>(m_begin));
+    assert(is_aligned_for<T>(m_end));
   }
   iterator begin() {
     return m_begin;
@@ -53,19 +62,36 @@ class MetaDataRange {
   }
 };
 
-class DataRange {
-  unsigned char* m_begin;
-  unsigned char* m_end;
+template<typename T>
+class Buffer {
+ public:
+  using value_type = T;
+  using iterator = T*;
+  using const_iterator = T const*;
+
+ private:
+  iterator m_begin;
+  iterator m_end;
+  iterator m_next_read;
+  iterator m_next_write;
 
  public:
-  using value_type = unsigned char;
-  using iterator = unsigned char*;
-  using const_iterator = unsigned char const*;
-  DataRange(unsigned char* begin, unsigned char* end)
-      : m_begin(begin),
-        m_end(end) {
-    assert(is_aligned_for<EventHeader>(begin));
-    assert(is_aligned_for<EventHeader>(end));
+
+  Buffer(void* begin, void* end)
+      :
+        m_begin(pointer_cast<T>(begin)),
+        m_end(pointer_cast<T>(end)),
+        m_next_read(pointer_cast<T>(begin)),
+        m_next_write(pointer_cast<T>(begin)) {
+    assert(m_begin < m_end);
+  }
+  Buffer(iterator begin, iterator end)
+      :
+        m_begin(begin),
+        m_end(end),
+        m_next_read(begin),
+        m_next_write(begin) {
+    assert(m_begin < m_end);
   }
   iterator begin() {
     return m_begin;
@@ -73,122 +99,38 @@ class DataRange {
   iterator end() {
     return m_end;
   }
-  const_iterator begin() const {
-    return m_begin;
+  iterator next_read() {
+    return m_next_read;
   }
-  const_iterator end() const {
-    return m_end;
+  iterator next_write() {
+    return m_next_write;
+  }
+  size_t size() {
+    return std::distance(m_begin, m_end);
+  }
+  size_t ready() {
+    return (size() + std::distance(m_next_read, m_next_write)) % size();
+  }
+  size_t available() {
+    return size() - ready();
+  }
+  void release(size_t n) {
+    assert(ready() >= n);
+    m_next_read = m_begin + (m_next_read - m_begin + n) % size();
+  }
+  void reserve(size_t n) {
+    assert(available() > n);
+    m_next_write = m_begin + (m_next_write - m_begin + n) % size();
   }
 };
+
+using MetaDataRange = Range<EventMetaData>;
+using DataRange = Range<unsigned char>;
+
+using MetaDataBuffer = Buffer<EventMetaData>;
+using DataBuffer = Buffer<unsigned char>;
 
 using MultiEvents = std::vector<std::pair<MetaDataRange, DataRange> >;
-
-class MetaDataBuffer {
-  EventMetaData* m_begin;
-  EventMetaData* m_end;
-  EventMetaData* m_next_read;
-  EventMetaData* m_next_write;
-
- public:
-  using value_type = EventMetaData;
-  using iterator = EventMetaData*;
-  using const_iterator = EventMetaData const*;
-  MetaDataBuffer(unsigned char* begin, unsigned char* end)
-      : m_begin(pointer_cast<EventMetaData>(begin)),
-        m_end(pointer_cast<EventMetaData>(end)),
-        m_next_read(pointer_cast<EventMetaData>(begin)),
-        m_next_write(pointer_cast<EventMetaData>(begin)) {
-    assert(m_begin < m_end);
-  }
-  MetaDataBuffer(EventMetaData* begin, EventMetaData* end)
-      : m_begin(begin),
-        m_end(end),
-        m_next_read(begin),
-        m_next_write(begin) {
-    assert(m_begin < m_end);
-  }
-  iterator begin() {
-    return m_begin;
-  }
-  iterator end() {
-    return m_end;
-  }
-  iterator next_read() {
-    return m_next_read;
-  }
-  iterator next_write() {
-    return m_next_write;
-  }
-  size_t size() {
-    return std::distance(m_begin, m_end);
-  }
-  size_t ready() {
-    return (size() + std::distance(m_next_read, m_next_write)) % size();
-  }
-  size_t available() {
-    return size() - ready();
-  }
-  void release(size_t n_events) {
-    assert(ready() >= n_events);
-    m_next_read = m_begin + (m_next_read - m_begin + n_events) % size();
-  }
-  void reserve(size_t n_events) {
-    assert(available() > n_events);
-    m_next_write = m_begin + (m_next_write - m_begin + n_events) % size();
-  }
-};
-
-class DataBuffer {
-  unsigned char* m_begin;
-  unsigned char* m_end;
-  unsigned char* m_next_read;
-  unsigned char* m_next_write;
-
- public:
-  using value_type = unsigned char;
-  using iterator = unsigned char*;
-  using const_iterator = unsigned char const*;
-  DataBuffer(unsigned char* begin, unsigned char* end)
-      : m_begin(begin),
-        m_end(end),
-        m_next_read(begin),
-        m_next_write(begin) {
-    assert(is_aligned_for<EventHeader>(begin));
-    assert(is_aligned_for<EventHeader>(end));
-    assert(m_begin < m_end);
-  }
-  iterator begin() {
-    return m_begin;
-  }
-  iterator end() {
-    return m_end;
-  }
-  iterator next_read() {
-    return m_next_read;
-  }
-  iterator next_write() {
-    return m_next_write;
-  }
-  size_t size() {
-    return std::distance(m_begin, m_end);
-  }
-  size_t ready() {
-    return (size() + std::distance(m_next_read, m_next_write)) % size();
-  }
-  size_t available() {
-    return size() - ready();
-  }
-  void release(size_t n_bytes) {
-    assert(ready() >= n_bytes);
-    m_next_read = m_begin + (m_next_read - m_begin + n_bytes) % size();
-    assert(is_aligned_for<EventHeader>(m_next_read));
-  }
-  void reserve(size_t n_bytes) {
-    assert(available() > n_bytes);
-    m_next_write = m_begin + (m_next_write - m_begin + n_bytes) % size();
-    assert(is_aligned_for<EventHeader>(m_next_write));
-  }
-};
 
 }
 
