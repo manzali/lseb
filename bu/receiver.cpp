32455014a867be17360dda9f0aa4c4a3e516869c
Matteo Manzali <matteo.manzali@gmail.com>
2015-03-20 15:57:44 +0100

    Style changes in receiver.

--- bu/receiver.cpp
+++ bu/receiver.cpp
@@ -7,58 +7,68 @@
 
 namespace lseb {
 
-Receiver::Receiver(MetaDataRange const& metadata_range,
-                   DataRange const& data_range, size_t events_in_multievent,
-                   std::vector<int> const& connection_ids)
-    : m_metadata_range(metadata_range),
+Receiver::Receiver(
+  MetaDataRange const& metadata_range,
+  DataRange const& data_range,
+  size_t events_in_multievent,
+  std::vector<int> const& connection_ids)
+    :
+      m_metadata_range(metadata_range),
       m_data_range(data_range),
-      m_events_in_multievent(events_in_multievent),
-      m_multievent_size(events_in_multievent * sizeof(EventMetaData)) {
+      m_events_in_multievent(events_in_multievent) {
   // Prepare the vector of file descriptors to be polled
   std::transform(
-      std::begin(connection_ids),
-      std::end(connection_ids),
-      std::back_inserter(m_poll_fds),
-      [](int id) {return pollfd {id, POLLIN, 0};}
-  );
+    std::begin(connection_ids),
+    std::end(connection_ids),
+    std::back_inserter(m_poll_fds),
+    [](int id) {return pollfd {id, POLLIN, 0};});
 }
 
-size_t Receiver::receive(int timeout_ms){
+size_t Receiver::receive(int timeout_ms) {
+
+  size_t const multievent_size = m_events_in_multievent * sizeof(EventMetaData);
 
   int nevents = lseb_poll(m_poll_fds, timeout_ms);
   size_t read_bytes = 0;
 
-  for (auto it = m_poll_fds.begin(); nevents > 0 && it != m_poll_fds.end();
-      ++it) {
+  for (auto it = std::begin(m_poll_fds);
+      nevents > 0 && it != std::end(m_poll_fds); ++it) {
+
     if (it->revents & POLLIN) {
 
       // Read metadata
-      size_t const offset = std::distance(m_poll_fds.begin(), it)
-          * m_events_in_multievent;
+      size_t const offset =
+        std::distance(std::begin(m_poll_fds), it) * m_events_in_multievent;
+
+      assert(
+        std::begin(m_metadata_range) + offset + m_events_in_multievent <= std::end(
+          m_metadata_range));
+
       MetaDataRange multievent_meta(
-          m_metadata_range.begin() + offset,
-          m_metadata_range.begin() + offset + m_events_in_multievent);
+        std::begin(m_metadata_range) + offset,
+        std::begin(m_metadata_range) + offset + m_events_in_multievent);
 
-      ssize_t read_meta = lseb_read(it->fd, multievent_meta.begin(), m_multievent_size);
-      assert(read_meta >= 0 && static_cast<size_t>(read_meta) == m_multievent_size);
-
-      LOG(DEBUG) << "Read " << read_meta << " / " << m_multievent_size
-                 << " bytes";
+      ssize_t read_meta = lseb_read(
+        it->fd,
+        std::begin(multievent_meta),
+        multievent_size);
+      assert(
+        read_meta >= 0 && static_cast<size_t>(read_meta) == multievent_size);
 
       // Read data
       size_t data_load = std::accumulate(
-          std::begin(multievent_meta),
-          std::end(multievent_meta),
-          0,
-          [](size_t partial, EventMetaData const& meta) {
-            return partial + meta.length;
-          }
-      );
+        std::begin(multievent_meta),
+        std::end(multievent_meta),
+        0,
+        [](size_t partial, EventMetaData const& meta) {
+          return partial + meta.length;
+        });
 
-      ssize_t read_data= lseb_read(it->fd, m_data_range.begin(), data_load);
+      ssize_t read_data = lseb_read(
+        it->fd,
+        std::begin(m_data_range),
+        data_load);
       assert(read_data >= 0 && static_cast<size_t>(read_data) == data_load);
-
-      LOG(DEBUG) << "Read " << read_data << " / " << data_load << " bytes";
 
       read_bytes += read_meta;
       read_bytes += read_data;
--- bu/receiver.h
+++ bu/receiver.h
@@ -1,6 +1,8 @@
 #ifndef BU_RECEIVER_H
 #define BU_RECEIVER_H
 
+#include <vector>
+
 #include <sys/poll.h>
 
 #include "common/dataformat.h"
@@ -11,12 +13,14 @@ class Receiver {
   MetaDataRange m_metadata_range;
   DataRange m_data_range;
   size_t m_events_in_multievent;
-  size_t m_multievent_size;
   std::vector<pollfd> m_poll_fds;
 
  public:
-  Receiver(MetaDataRange const& metadata_range, DataRange const& data_range,
-           size_t events_in_multievent, std::vector<int> const& connection_ids);
+  Receiver(
+    MetaDataRange const& metadata_range,
+    DataRange const& data_range,
+    size_t events_in_multievent,
+    std::vector<int> const& connection_ids);
   size_t receive(int timeout_ms);
 };
 
