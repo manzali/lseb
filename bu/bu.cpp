55fa411664f28ffc6a7a873ca6ac1358d150278b
Matteo Manzali <matteo.manzali@gmail.com>
2015-07-07 17:18:27 +0200

    Little improvement.

--- bu/bu.cpp
+++ bu/bu.cpp
@@ -100,8 +100,9 @@ int main(int argc, char* argv[]) {
 
     std::map<int, std::vector<void*> > wrs_map;
     for (auto const& m : iov_map) {
+      auto& it = wrs_map.insert(std::make_pair(m.first, std::vector<void*>()));
       for (auto const& i : m.second) {
-        wrs_map[m.first].push_back(i.iov_base);
+        it.first->second.push_back(i.iov_base);
       }
     }
 
