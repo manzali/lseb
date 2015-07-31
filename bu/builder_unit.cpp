d870f36b07b7e9f079a3543f2d9c21aad5c6b588
Matteo Manzali <matteo.manzali@gmail.com>
2015-07-31 16:17:23 +0200

    Minor changes.

--- bu/builder_unit.cpp
+++ bu/builder_unit.cpp
@@ -121,7 +121,7 @@ void BuilderUnit::operator()() {
       }
       else{
         LOG(WARNING) << "No wrs from conn " << m.first;
-        std::this_thread::sleep_for(std::chrono::microseconds(1));
+        std::this_thread::sleep_for(std::chrono::microseconds(10));
       }
       min_wrs = (min_wrs < m.second.size()) ? min_wrs : m.second.size();
     }
