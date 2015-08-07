2b55d387f869d997b17826f14ade52eceed69d27
Matteo Manzali <matteo.manzali@gmail.com>
2015-08-07 17:09:23 +0200

    Fixed bu connection cycle..

--- ru/readout_unit.cpp
+++ ru/readout_unit.cpp
@@ -103,9 +103,7 @@ void ReadoutUnit::operator()() {
         lseb_connect(ep.hostname(), ep.port(), tokens));
       RuConnectionId& conn = p.first->second;
       lseb_register(conn, m_id, data_ptr.get(), data_size);
-      LOG(NOTICE)
-        << "Readout Unit - Connection established with bu "
-        << conn.id;
+      LOG(NOTICE) << "Readout Unit - Connection established with bu " << id;
     }
   }
   LOG(NOTICE) << "Readout Unit - All connections established";
