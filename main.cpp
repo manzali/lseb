324319bda0f06827ae6d55532b9640d56dc953df
Matteo Manzalli <matteo.manzali@gmail.com>
2016-02-16 19:41:32 +0100

    Added tcp_barrier

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include "common/log.hpp"
 #include "common/configuration.h"
 #include "common/dataformat.h"
+#include "common/tcp_barrier.h"
 
 #include "transport/endpoints.h"
 
@@ -176,6 +177,12 @@ int main(int argc, char* argv[]) {
 
   bu_conn_th.join();
   ru_conn_th.join();
+
+  tcp_barrier(
+    id,
+    endpoints.size(),
+    endpoints[0].hostname(),
+    endpoints[0].port());
 
   std::thread bu_th(&BuilderUnit::run, &bu);
   std::thread ru_th(&ReadoutUnit::run, &ru);
