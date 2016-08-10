b5693ed235d73024d0b7b99d713d114e6188519c
valentino <valentino.picotti@gmail.com>
2016-08-10 10:41:22 +0200

    Type traits for libfabric types

---
+++ transport/libfabric/traits.h
@@ -0,0 +1,31 @@
+//
+// Created by Valentino on 10/08/16.
+//
+
+#ifndef TRANSPORT_LIBFABRIC_FABRIC_TRAITS_H
+#define TRANSPORT_LIBFABRIC_FABRIC_TRAITS_H
+
+#include <type_traits>
+#include <rdma/fabric.h>
+
+namespace lseb {
+
+namespace detail {
+template<typename T>
+struct _is_fabric_id :
+public std::false_type {
+};
+
+template<>
+struct _is_fabric_id<fid_mr> :
+public std::true_type {
+};
+}
+
+template<typename T>
+struct is_fabric_id
+    : public detail::_is_fabric_id<typename std::remove_cv<T>::type> {
+};
+}
+
+#endif //TRANSPORT_LIBFABRIC_FABRIC_TRAITS_H
