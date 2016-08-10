0df686383a50be42e4511902dad6e937546f7d6f
valentino <valentino.picotti@gmail.com>
2016-08-10 10:42:47 +0200

    Socket implementation with libfabric

---
+++ transport/libfabric/socket.cpp
@@ -0,0 +1,251 @@
+#include "transport/libfabric/socket.h"
+
+#include <arpa/inet.h>
+#include <rdma/fi_cm.h>
+#include <rdma/fi_errno.h>
+#include "common/exception.h"
+
+namespace {
+bool is_in_mr(const iovec &iov, const lseb::Socket::memory_region &mr) {
+  bool res = mr.first->mem_desc <= iov.iov_base
+      && static_cast<char *>(mr.first->mem_desc) + mr.second
+          >= static_cast<char *>(iov.iov_base) + iov.iov_len;
+  return res;
+}
+}
+
+namespace lseb {
+
+Socket::Socket(fid_domain *ac, fid_ep *ep, fid_cq *rx_cq,
+                   fid_cq *tx_cq, uint32_t credits)
+    : m_ac(ac), m_ep(ep), m_rx_cq(rx_cq), m_tx_cq(tx_cq), m_credits(credits) {
+}
+
+Socket::~Socket() {
+
+}
+
+void Socket::register_memory(void *buffer, size_t size) {
+  fid_mr *mr;
+  auto ret = fi_mr_reg(m_ac,
+                       buffer,
+                       size,
+                       FI_SEND | FI_RECV,
+                       0 /* offset, must be 0 */,
+                       0 /* requested key */,
+                       0 /* flags, must be 0 */,
+                       &mr,
+                       0 /* context */);
+
+  if (ret) {
+    throw exception::socket::generic_error(
+        "Error on fi_mr_reg: "
+            + std::string(fi_strerror(static_cast<int>(-ret))));
+  }
+
+  m_mrs.emplace_back(mr_ptr(mr), size);
+}
+
+std::vector<iovec> Socket::poll_completed_send() {
+  std::vector<fi_cq_entry> wcs(m_credits);
+
+  auto ret = fi_cq_read(m_tx_cq, wcs.data(), wcs.size());
+
+  std::vector<iovec> iov_vect{};
+  fi_cq_err_entry cq_err;
+  const char *err = nullptr;
+
+  if (ret < 0) {
+    switch (ret) {
+    case -FI_EAGAIN:
+      return iov_vect;
+    case -FI_EAVAIL:
+      fi_cq_readerr(m_tx_cq, &cq_err, 0 /* flags not documented */);
+      err = fi_cq_strerror(m_tx_cq,
+                           cq_err.err,
+                           cq_err.err_data,
+                           nullptr,
+                           0);
+      break;
+    default:
+      // Seams to be impossible to reach this label, maybe remove it
+      err = fi_strerror(static_cast<int>(-ret));
+    }
+    throw exception::socket::generic_error(
+        "Error on fi_cq_read: " + std::string(err));
+  }
+
+  iov_vect.reserve(static_cast<decltype(iov_vect)::size_type > (ret));
+
+  // Note: Is safe to store the map_end iterator because the erase function
+  //       invalidates only references and iterators to the erased element
+  auto map_end = std::end(m_pending_send);
+  for (auto it = std::begin(wcs), end = it + ret; it!=end; ++it) {
+    auto map_it = m_pending_send.find(it->op_context);
+    if (map_it==map_end) {
+      throw exception::socket::generic_error(
+          "Error on find: key element not exists");
+    }
+    iov_vect.push_back({map_it->first, map_it->second});
+    m_pending_send.erase(map_it);
+  }
+
+  return iov_vect;
+}
+
+std::vector<iovec> Socket::poll_completed_recv() {
+  std::vector<fi_cq_entry> wcs(m_credits);
+
+  auto ret = fi_cq_read(m_rx_cq, wcs.data(), wcs.size());
+
+  std::vector<iovec> iov_vect{};
+  fi_cq_err_entry cq_err;
+  const char *err = nullptr;
+
+  if (ret < 0) {
+    switch (ret) {
+    case -FI_EAGAIN:
+      return iov_vect;
+    case -FI_EAVAIL:
+      fi_cq_readerr(m_rx_cq, &cq_err, 0 /* flags not documented */);
+      err = fi_cq_strerror(m_rx_cq,
+                           cq_err.err,
+                           cq_err.err_data,
+                           nullptr,
+                           0);
+      break;
+    default:
+      // Seams to be impossible to reach this label, maybe remove it
+      err = fi_strerror(static_cast<int>(-ret));
+    }
+    throw exception::socket::generic_error(
+        "Error on fi_cq_read: " + std::string(err));
+  }
+
+  iov_vect.reserve(static_cast<decltype(iov_vect)::size_type > (ret));
+
+  // Note: Is safe to store the map_end iterator because the erase function
+  //       invalidates only references and iterators to the erased element
+  auto map_end = std::end(m_pending_recv);
+  for (auto it = std::begin(wcs), end = it + ret; it!=end; ++it) {
+    auto map_it = m_pending_recv.find(it->op_context);
+    if (map_it==map_end) {
+      throw exception::socket::generic_error(
+          "Error on find: key element not exists");
+    }
+    iov_vect.push_back({map_it->first, map_it->second});
+    m_pending_recv.erase(map_it);
+  }
+
+  return iov_vect;
+}
+
+void Socket::post_send(iovec const &iov) {
+  if (fi_tx_size_left(m_ep) <= 0) {
+    throw exception::socket::generic_error(
+        "Error on post_send: no credits available");
+  }
+
+  auto mr_it = std::find_if(std::begin(m_mrs),
+                            std::end(m_mrs),
+                            [&iov](decltype(m_mrs)
+                                   ::const_reference m) -> bool {
+                              return is_in_mr(iov, m);
+                            });
+
+  if (mr_it==std::end(m_mrs)) {
+    throw exception::socket::generic_error(
+        "Error on find_if: no valid memory region found");
+  }
+
+  auto ret = fi_send(m_ep,
+                     iov.iov_base,
+                     iov.iov_len,
+                     fi_mr_desc(mr_it->first.get()),
+                     0, /* dest_address */
+                     iov.iov_base);/* context */
+  if (ret) {
+    throw exception::socket::generic_error(
+        "Error on fi_recv: "
+            + std::string(fi_strerror(static_cast<int>(-ret))));
+  }
+
+  auto p = m_pending_send.emplace(iov.iov_base, iov.iov_len);
+  if (!p.second) {
+    throw exception::socket::generic_error(
+        "Error on insert: key element already exists");
+  }
+}
+
+void Socket::post_recv(iovec const &iov) {
+  if (fi_rx_size_left(m_ep) <= 0) {
+    throw exception::socket::generic_error(
+        "Error on post_send: no credits available");
+  }
+
+  auto mr_it = std::find_if(std::begin(m_mrs),
+                            std::end(m_mrs),
+                            [&iov](decltype(m_mrs)
+                                   ::const_reference m) -> bool {
+                              return is_in_mr(iov, m);
+                            });
+
+  if (mr_it==std::end(m_mrs)) {
+    throw exception::socket::generic_error(
+        "Error on find_if: no valid memory region found");
+  }
+
+  auto ret = fi_recv(m_ep,
+                     iov.iov_base,
+                     iov.iov_len,
+                     fi_mr_desc(mr_it->first.get()),
+                     0, /* src_address */
+                     iov.iov_base);/* context */
+  if (ret) {
+    throw exception::socket::generic_error(
+        "Error on fi_recv: "
+            + std::string(fi_strerror(static_cast<int>(-ret))));
+  }
+
+  auto p = m_pending_recv.emplace(iov.iov_base, iov.iov_len);
+  if (!p.second) {
+    throw exception::socket::generic_error(
+        "Error on insert: key element already exists");
+  }
+
+}
+
+bool Socket::available_send() {
+  return fi_rx_size_left(m_ep) > 0;
+}
+
+bool Socket::available_recv() {
+  return fi_tx_size_left(m_ep) > 0;
+}
+
+std::vector<iovec> Socket::pending_send() {
+  std::vector<iovec> iov_vect;
+  for (auto const &pair : m_pending_send) {
+    iov_vect.push_back({pair.first, pair.second});
+  }
+  return iov_vect;
+}
+
+std::vector<iovec> Socket::pending_recv() {
+  std::vector<iovec> iov_vect;
+  for (auto const &pair : m_pending_send) {
+    iov_vect.push_back({pair.first, pair.second});
+  }
+  return iov_vect;
+}
+
+std::string Socket::peer_hostname() {
+  char str[INET_ADDRSTRLEN];
+  size_t len = sizeof(sockaddr_in);
+  sockaddr_in addr[1];
+  fi_getpeer(m_ep, reinterpret_cast<void *>(addr), &len);
+  inet_ntop(AF_INET, &(addr->sin_addr), str, INET_ADDRSTRLEN);
+  return str;
+}
+
+}
---
+++ transport/libfabric/socket.h
@@ -0,0 +1,89 @@
+#ifndef TRANSPORT_VERBS_SOCKET_H
+#define TRANSPORT_VERBS_SOCKET_H
+
+#include <memory>
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <tuple>
+
+#include <cstdint>
+
+#include <rdma/fabric.h>
+#include "shared.h"
+
+namespace lseb {
+
+class Socket {
+public:
+
+  typedef std::unique_ptr<fid_mr, lseb::delete_fid<fid_mr>> mr_ptr;
+  typedef std::pair<mr_ptr, uint64_t> memory_region;
+
+  Socket(fid_domain *ac,
+           fid_ep *ep,
+           fid_cq *rx_cq,
+           fid_cq *tx_cq,
+           uint32_t credits);
+  Socket(Socket const &other) = delete;
+  Socket &operator=(Socket const &) = delete;
+  ~Socket();
+
+  void register_memory(void *buffer, size_t size);
+
+  std::vector<iovec> poll_completed_send();
+  std::vector<iovec> poll_completed_recv();
+
+  void post_send(iovec const &iov);
+  void post_recv(iovec const &iov);
+
+  bool available_send();
+  bool available_recv();
+
+  std::vector<iovec> pending_send();
+  std::vector<iovec> pending_recv();
+
+  std::string peer_hostname();
+
+private:
+
+  // TODO: Decide ownership of these resources
+  // TODO: Order of declaration is important in deconstruction: order members
+
+  /* Access Domain
+   * Owned by the c/a (connector/acceptor) but needed by fi_mr_reg
+   * TODO: Use shared_ptr
+   */
+  fid_domain *m_ac;
+
+  /* Endpoint
+   * Allocated by c/a, owned by socket
+   * TODO: Use unique_ptr
+   */
+  fid_ep *m_ep;
+
+  /* Completion Queues:
+   * Allocated and owned by socket
+   * TODO: Use unique_ptr
+   * Note: An endpoint that will generate asynchronous completions, either
+   * through data transfer operations or communication establishment events,
+   * must be bound to the appropriate completion queues or event queues before
+   * being enabled.
+   * TODO: Bind before ep activation
+   * Note: The fi_connect and fi_accept calls will also transition an endpoint into
+   * the enabled state, if it is not already active.
+   */
+  fid_cq *m_rx_cq; // Receive CQ
+  fid_cq *m_tx_cq; // Transmit CQ
+
+  uint32_t m_credits; // CQ Depth
+  std::vector<memory_region> m_mrs;
+  // TODO: Remove the unordered_map because we let the application store the
+  //       length of the registered buffers. Maybe an unordered_set is needed
+  std::unordered_map<void *, size_t> m_pending_send;
+  std::unordered_map<void *, size_t> m_pending_recv;
+
+};
+
+}
+#endif
