9308310d76643bf28ef58fa7ef5fe96fcd023768
valentino <valentino.picotti@gmail.com>
2016-08-29 18:50:19 +0200

    Refactored connect function

--- transport/libfabric/connector.cpp
+++ transport/libfabric/connector.cpp
@@ -2,6 +2,7 @@
 
 #include <cstring>
 
+#include <rdma/fi_errno.h>
 #include "rdma/fi_endpoint.h"
 #include "rdma/fi_cm.h"
 
@@ -20,115 +21,63 @@ std::unique_ptr<Socket> Connector::connect(
   // TODO: Use unique_ptr on all fabric resources to avoid memory leaks on
   //       connection failure
   // TODO: Improve error reporting (and avoid exceptions?)
-  fi_info *info;
-
   struct fi_eq_cm_entry entry;
-  uint32_t event;
   int rc = 0;
+  fi_info *info_p;
+  fid_ep *ep;
+  fid_cq *rx_cq, *tx_cq;
+  fid_eq *eq;
+  Domain& d = Domain::get_instance();
+
+  uint32_t event;
+
   ssize_t rd;
 
-  Domain& d = Domain::get_instance();
 
   /* Resolve address to a fabric specific one */
   // TODO: Verify address correctness in info->dest_addrs
-  fi_getinfo(FI_VERSION(1, 3),
-             hostname.c_str(),
-             port.c_str(),
-             0,
-             d.get_hints(),
-             &info);
+  rc = fi_getinfo(FI_VERSION(1, 3),
+                  hostname.c_str(),
+                  port.c_str(),
+                  0,
+                  d.get_hints(),
+                  &info_p);
 
-  fid_eq *eq;
-  /* EQ create */
-  struct fi_eq_attr cm_attr;
-
-  std::memset(&cm_attr, 0, sizeof cm_attr);
-  cm_attr.wait_obj = FI_WAIT_FD;
-
-  rc = fi_eq_open(d.get_raw_fabric(), &cm_attr, &eq, NULL);
   if (rc) {
-    //FT_PRINTERR("fi_eq_open", rc);
+    throw exception::connector::generic_error(
+        "Error on fi_getinfo: " + std::string(fi_strerror(-rc)));
   }
 
-  /* End CQ create*/
-  fid_ep *ep;
-  /* Open endpoint */
-  rc = fi_endpoint(d.get_raw_domain(), info, &ep, NULL);
+  std::unique_ptr<fi_info, fid_deleter<fi_info>> info{info_p};
+
+  rc = fi_endpoint(d.get_raw_domain(), info.get(), &ep, NULL);
+
   if (rc) {
-    //FT_PRINTERR("fi_endpoint", rc);
-    return nullptr;
+    throw exception::connector::generic_error(
+        "Error on fi_endpoint: " + std::string(fi_strerror(-rc)));
   }
 
-  fid_cq *rx_cq;
-  fid_cq *tx_cq;
-  /* Create event queue */
-  struct fi_cq_attr cq_attr;
-  std::memset(&cq_attr, 0, sizeof cq_attr);
-  cq_attr.format = FI_CQ_FORMAT_CONTEXT;
+  // Create completion queues
+  bind_completion_queues(ep, &rx_cq, &tx_cq, m_credits);
+  // Create event queues
+  bind_event_queue(ep, &eq);
 
-  cq_attr.wait_obj = FI_WAIT_FD;
-  cq_attr.size = m_credits + 1;
-
-  rc = fi_cq_open(d.get_raw_domain(), &cq_attr, &rx_cq, NULL);
-  if (rc) {
-    // FT_PRINTERR("fi_cq_open", rc);
-    return nullptr;
-  }
-
-  rc = fi_cq_open(d.get_raw_domain(), &cq_attr, &tx_cq, NULL);
-  if (rc) {
-    // FT_PRINTERR("fi_cq_open", rc);
-    return nullptr;
-  }
-
-  /* Bind eq to ep */
-  rc = fi_ep_bind(ep, &rx_cq->fid, FI_RECV);
-  if (rc) {
-    // FT_PRINTERR("fi_ep_bind", rc);
-    return nullptr;
-  }
-
-  rc = fi_ep_bind(ep, &tx_cq->fid, FI_SEND);
-  if (rc) {
-    // FT_PRINTERR("fi_ep_bind", rc);
-    return nullptr;
-  }
-
-  rc = fi_ep_bind(ep, &eq->fid, 0);
-  if (rc) {
-    // FT_PRINTERR("fi_ep_bind", rc);
-    return nullptr;
-  }
-
+  // TODO: Remove this function call because fi_connect already enables the ep
   rc = fi_enable(ep);
   if (rc) {
-    // FT_PRINTERR("fi_enable", rc);
-    return nullptr;
+    throw exception::connector::generic_error(
+        "Error on fi_enable: " + std::string(fi_strerror(-rc)));
   }
-
-  printf("Connecting to server\n");
 
   rc = fi_connect(ep, info->dest_addr, NULL, 0);
-  fi_freeinfo(info);
   if (rc) {
-    // FT_PRINTERR("fi_connect", rc);
-    return nullptr;
+    throw exception::connector::generic_error(
+        "Error on fi_connect: " + std::string(fi_strerror(-rc)));
   }
 
-  rd = fi_eq_sread(eq, &event, &entry, sizeof entry, -1, 0);
-  if (rd!=sizeof entry) {
-    // FT_PROCESS_EQ_ERR(rd, ctx->eq, "fi_eq_sread", "connect");
-    return nullptr;
-  }
+  read_event(eq, &entry, FI_CONNECTED);
 
-  if (event!=FI_CONNECTED) {
-    fprintf(stderr, "Unexpected CM event %d\n", event);
-    return nullptr;
-  }
-
-  printf("Connection successful\n");
-  // std::unique_ptr<Socket> socket_ptr(new Socket(cm_id, m_credits));
-  return lseb::make_unique<Socket>(ep, rx_cq, tx_cq, m_credits);
+  return make_unique<Socket>(ep, rx_cq, tx_cq, m_credits);
 }
 
 }
