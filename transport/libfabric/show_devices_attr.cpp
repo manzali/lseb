06fee904717ac2c6ed35017619a698495fde4cee
Matteo Manzali <matteo.manzali@gmail.com>
2017-04-14 12:10:27 +0200

    Complete show_devices_attr

--- transport/libfabric/show_devices_attr.cpp
+++ transport/libfabric/show_devices_attr.cpp
@@ -9,31 +9,73 @@ int main() {
 
   for (fi_info *it = info; it != nullptr; it = it->next) {
 
-    std::cout << "available service:\n";
+    std::cout << "\navailable service:\n"
+              << fi_tostr(it, FI_TYPE_INFO)
+              << std::endl;
 
-    // print provider name
-    std::cout << "\tprovider name: " << it->fabric_attr->prov_name << std::endl;
+/*
+    std::cout << "    control_progress: "
+              << fi_tostr(&it->domain_attr->control_progress, FI_TYPE_PROGRESS)
+              << std::endl;
 
-    // print fabric identifier
-    std::cout << "\tfabric identifier: " << it->fabric_attr->name << std::endl;
-
-    // print version
-    std::cout << "\tversion: "
+    std::cout << "  fabric_attr:\n";
+    std::cout << "    name: " << it->fabric_attr->name << std::endl;
+    std::cout << "    prov_name: " << it->fabric_attr->prov_name << std::endl;
+    std::cout << "    prov_version: "
               << FI_MAJOR(it->fabric_attr->prov_version)
               << "."
               << FI_MINOR(it->fabric_attr->prov_version)
               << std::endl;
 
-    // print endpoint type
-    std::cout << "\tendpoint type: "
+    std::cout << "  domain_attr:\n";
+    std::cout << "    name: " << it->domain_attr->name << std::endl;
+    std::cout << "    threading: "
+              << fi_tostr(&it->domain_attr->threading, FI_TYPE_THREADING)
+              << std::endl;
+    std::cout << "    control_progress: "
+              << fi_tostr(&it->domain_attr->control_progress, FI_TYPE_PROGRESS)
+              << std::endl;
+    std::cout << "    data_progress: "
+              << fi_tostr(&it->domain_attr->data_progress, FI_TYPE_PROGRESS)
+              << std::endl;
+    std::cout << "    av_type: "
+              << fi_tostr(&it->domain_attr->av_type, FI_TYPE_AV_TYPE)
+              << std::endl;
+
+    std::cout << "    mr_key_size: "
+              << it->domain_attr->mr_key_size
+              << std::endl;
+    std::cout << "    cq_data_size: "
+              << it->domain_attr->cq_data_size
+              << std::endl;
+    std::cout << "    cq_cnt: " << it->domain_attr->cq_cnt << std::endl;
+    std::cout << "    ep_cnt: " << it->domain_attr->ep_cnt << std::endl;
+    std::cout << "    tx_ctx_cnt: " << it->domain_attr->tx_ctx_cnt << std::endl;
+    std::cout << "    rx_ctx_cnt: " << it->domain_attr->rx_ctx_cnt << std::endl;
+    std::cout << "    max_ep_tx_ctx: "
+              << it->domain_attr->max_ep_tx_ctx
+              << std::endl;
+    std::cout << "    max_ep_rx_ctx: "
+              << it->domain_attr->max_ep_rx_ctx
+              << std::endl;
+    std::cout << "    max_ep_stx_ctx: "
+              << it->domain_attr->max_ep_stx_ctx
+              << std::endl;
+    std::cout << "    max_ep_srx_ctx: "
+              << it->domain_attr->max_ep_srx_ctx
+              << std::endl;
+
+    std::cout << "  ep_attr:\n";
+    std::cout << "    type: "
               << fi_tostr(&it->ep_attr->type, FI_TYPE_EP_TYPE)
               << std::endl;
-
-    // print protocol
-    std::cout << "\tprotocol: "
+    std::cout << "    protocol: "
               << fi_tostr(&it->ep_attr->protocol, FI_TYPE_PROTOCOL)
               << std::endl;
-
+    std::cout << "    protocol: "
+              << fi_tostr(&it->ep_attr->protocol, FI_TYPE_PROTOCOL)
+              << std::endl;
+*/
   }
   fi_freeinfo(info);
 
