a73a72f789070dd1685fc590e40a83e92be06eeb
Matteo Manzali <matteo.manzali@gmail.com>
2017-04-18 12:21:29 +0200

    Fix

--- utils/check_counters.py
+++ utils/check_counters.py
@@ -3,19 +3,29 @@
 import sys
 import subprocess
 import time
+from datetime import datetime
 
 interval = 5.0 # time interval in seconds
 
-p = subprocess.Popen(["/usr/sbin/perfquery", "-x", "-R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-out, err = p.communicate()
-
-if err:
-    sys.exit(err)
-
 while True:
 
+    # reset counters
+
+    p = subprocess.Popen(["/usr/sbin/perfquery", "-x", "-R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    out, err = p.communicate()
+
+    if err:
+        sys.exit(err)
+
+    # sleep
+
+    start = datetime.now()
     time.sleep(interval)
+    end = datetime.now()
+    delta_t = end - start
+
+    # read counters
 
     p = subprocess.Popen(["/usr/sbin/perfquery", "-x", "-r"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
@@ -24,9 +34,12 @@ while True:
     if err:
         sys.exit(err)
 
+    # split output
+
     output_list = out.split("\n")
 
     # Get Xmit and Rcv
+    
     xmit = long(output_list[3].split("PortXmitData:....................", 1)[1])
     rcv = long(output_list[4].split("PortRcvData:.....................", 1)[1])
 
@@ -39,8 +52,8 @@ while True:
     xmit = xmit * 8.0 # from byte to bit
     rcv = rcv * 8.0 # from byte to bit
 
-    xmit = xmit / interval # in time interval
-    rcv = rcv / interval # in time interval
+    xmit = xmit / delta_t # in time interval
+    rcv = rcv / delta_t # in time interval
 
     print "Xmit: {} Gb/s".format(xmit)
     print "Rcv:  {} Gb/s".format(rcv)
